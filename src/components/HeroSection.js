39bb6fa21b5eec7512a520a9e01bda114964dc93
Davis06 <davis06@outlook.com>
2023-11-21 18:24:02 +0100

    completed task

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,9 +3,9 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <section className='hero-section pl-[29px] lg:hidden'>
-        <div className='mb-[10px] flex gap-4'>
-            <img src='images/hero-banner.png'/>
-            <img src='images/hero-banner.png'/>
+        <div className='mb-[10px] flex gap-4 overflow-scroll'>
+            <img src='images/hero-banner.png' className='w-[333px]'/>
+            <img src='images/hero-banner.png' className='w-[333px]'/>
         </div>
         <div className='flex justify-center items-center gap-[4px] pb-[27px]'>
             <div className='dot rounded-[10px] bg-[#0F002B] h-[6px] w-[6px]'></div>
